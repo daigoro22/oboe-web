026234014bab59f70fcf947bdd4cb991e961d878
daigoro <daigoro.abe@systemi.co.jp>
2024-03-28 06:26:44 +0900

    fix: lint エラーを解消

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -2,7 +2,7 @@ import { jsxRenderer } from "hono/jsx-renderer";
 
 export const renderer = jsxRenderer(({ children, title }) => {
 	return (
-		<html>
+		<html lang="en">
 			<head>
 				<link href="/static/style.css" rel="stylesheet" />
 				<title>{title}</title>
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import build from '@hono/vite-cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
-import path from "path"
-import react from "@vitejs/plugin-react"
+import path from "node:path";
+import build from "@hono/vite-cloudflare-pages";
+import devServer from "@hono/vite-dev-server";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    build(),
-    react(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  ssr: { external: ["react", "react-dom"] },
-})
+	plugins: [
+		build(),
+		react(),
+		devServer({
+			adapter,
+			entry: "src/index.tsx",
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	ssr: { external: ["react", "react-dom"] },
+});
