9732e866a620258902e227d17d5126a7565487f0
daigoro <daigoro.abe@systemi.co.jp>
2024-03-31 16:33:50 +0900

    feat: hono で shadcn-ui のコンポーネントを描画できるようにした

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import { Hono } from "hono";
+import { Button } from "./components/ui/button";
 import { renderer } from "./renderer";
-
 const app = new Hono();
 
 app.use(renderer);
 
 app.get("/", (c) => {
-	return c.render(<h1>Hello!</h1>);
+	return c.render(
+		<div>
+			<Button>aaa</Button>
+			<h1>Hello!</h1>
+		</div>,
+	);
 });
 
 export default app;
