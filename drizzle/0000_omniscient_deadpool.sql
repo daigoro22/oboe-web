fc1952bd3bde370a24ba8d111b0494e812b8506c
daigoro <daigoro.abe@systemi.co.jp>
2024-04-03 07:58:04 +0900

    feat: drizzle のスキーマと最初のマイグレーション作成

---
+++ drizzle/0000_omniscient_deadpool.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `objectives` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text
+);
+--> statement-breakpoint
+CREATE TABLE `occupations` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`image` text,
+	`birth_date` integer NOT NULL,
+	`gender` text NOT NULL,
+	`occupation_id` integer NOT NULL,
+	`objective_id` integer NOT NULL,
+	`customer_id` text NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	FOREIGN KEY (`occupation_id`) REFERENCES `occupations`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`objective_id`) REFERENCES `objectives`(`id`) ON UPDATE no action ON DELETE no action
+);
---
+++ src/db/schema.ts
@@ -0,0 +1,46 @@
+import { sql } from "drizzle-orm";
+import {
+	foreignKey,
+	integer,
+	sqliteTable,
+	text,
+} from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable(
+	"users",
+	{
+		id: integer("id").primaryKey({ autoIncrement: true }),
+		name: text("name").notNull(),
+		image: text("image"),
+		birthDate: integer("birth_date", { mode: "timestamp" }).notNull(),
+		gender: text("gender", {
+			enum: ["男", "女", "その他", "無回答"],
+		}).notNull(),
+		occupationId: integer("occupation_id").notNull(),
+		objectiveId: integer("objective_id").notNull(),
+		customerId: text("customer_id").notNull(),
+		createdAt: integer("created_at", { mode: "timestamp_ms" })
+			.notNull()
+			.default(sql`CURRENT_TIMESTAMP`),
+	},
+	(users) => ({
+		occupationFk: foreignKey({
+			columns: [users.occupationId],
+			foreignColumns: [occupations.id],
+		}),
+		objectiveFk: foreignKey({
+			columns: [users.objectiveId],
+			foreignColumns: [objectives.id],
+		}),
+	}),
+);
+
+export const occupations = sqliteTable("occupations", {
+	id: integer("id").primaryKey({ autoIncrement: true }),
+	name: text("name"),
+});
+
+export const objectives = sqliteTable("objectives", {
+	id: integer("id").primaryKey({ autoIncrement: true }),
+	name: text("name"),
+});
