88a17a3cb17c398bd1dd956d096d5e3d29eeca42
daigoro <daigoro.abe@systemi.co.jp>
2024-04-05 09:25:16 +0900

    feat: シード投入できるようにした

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,4 +2,8 @@ import type { Config } from "drizzle-kit";
 export default {
 	schema: "./src/db/schema.ts",
 	out: "./migrations",
+	dbCredentials: {
+		wranglerConfigPath: "./wrangler.toml",
+		dbName: "oboe",
+	},
 } satisfies Config;
