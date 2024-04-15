cf49d8b380a54b3ac80b04195ac86d4ddf9a87cd
daigoro <daigoro.abe@systemi.co.jp>
2024-04-16 08:54:01 +0900

    feat(back): auth.js のセットアップ

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
-import { Hono } from "hono";
+import {
+	type AuthConfig,
+	authHandler,
+	initAuthConfig,
+	verifyAuth,
+} from "@hono/auth-js";
+import { type Context, Hono } from "hono";
+import { env } from "hono/adapter";
 import { Button } from "./components/ui/button";
 import { renderer } from "./renderer";
+
+import Line from "@auth/core/providers/line";
+import React from "react";
+
 const app = new Hono();
 
 app.use(renderer);
 
-app.get("/", (c) => {
+app.get("/", async (c) => {
 	return c.render(
 		<div>
 			<Button>aaa</Button>
@@ -13,5 +24,32 @@ app.get("/", (c) => {
 		</div>,
 	);
 });
+
+app.use("*", initAuthConfig(getAuthConfig));
+
+app.use("/api/auth/*", authHandler());
+
+app.use("/api/*", verifyAuth());
+
+app.get("/api/protected", (c) => {
+	const auth = c.get("authUser");
+	return c.json(auth);
+});
+
+function getAuthConfig(c: Context): AuthConfig {
+	const {
+		AUTH_SECRET: secret,
+		AUTH_LINE_ID: clientId,
+		AUTH_LINE_SECRET: clientSecret,
+	} = env<{
+		AUTH_SECRET: string;
+		AUTH_LINE_ID: string;
+		AUTH_LINE_SECRET: string;
+	}>(c);
+	return {
+		secret,
+		providers: [Line({ clientId, clientSecret })],
+	};
+}
 
 export default app;
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,12 +1,16 @@
+import { SessionProvider } from "@hono/auth-js/react";
 import { reactRenderer } from "@hono/react-renderer";
+import React from "react";
 
 export const renderer = reactRenderer(({ children }) => {
 	return (
-		<html lang="ja">
-			<head>
-				<link href="/src/globals.css" rel="stylesheet" />
-			</head>
-			<body>{children}</body>
-		</html>
+		<SessionProvider>
+			<html lang="ja">
+				<head>
+					<link href="/src/globals.css" rel="stylesheet" />
+				</head>
+				<body>{children}</body>
+			</html>
+		</SessionProvider>
 	);
 });
