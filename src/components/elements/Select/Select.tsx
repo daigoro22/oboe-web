7f0e8bfb89139b29c1270a9777f99cf3a496ad7a
daigoro <daigoro.abe@systemi.co.jp>
2024-05-02 09:09:22 +0900

    feat(front): Select コンポーネントに form モードを追加
    formatter かけた
    
    @hookform/resolvers を追加

--- src/components/elements/Select/Select.tsx
+++ src/components/elements/Select/Select.tsx
@@ -1,3 +1,4 @@
+import { FormControl } from "@/components/ui/form";
 import {
 	Select as ScSelect,
 	SelectContent,
@@ -5,20 +6,38 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
-import * as React from "react";
+import type * as React from "react";
 
 // biome-ignore lint/complexity/noBannedTypes: <explanation></explanation>
 export type SelectProps = {
 	id: string;
 	placeholder: string;
 	items: { value: string; label: string }[];
+	isForm?: boolean;
 };
-export const Select = ({ id, placeholder, items }: SelectProps) => {
+type ScSelectProps = React.ComponentProps<typeof ScSelect>;
+export const Select: React.FC<SelectProps & ScSelectProps> = ({
+	id,
+	placeholder,
+	items,
+	isForm = true,
+	...props
+}) => {
+	const Trigger = () => (
+		<SelectTrigger id={id}>
+			<SelectValue placeholder={placeholder} />
+		</SelectTrigger>
+	);
+
 	return (
-		<ScSelect>
-			<SelectTrigger id={id}>
-				<SelectValue placeholder={placeholder} />
-			</SelectTrigger>
+		<ScSelect {...props}>
+			{isForm ? (
+				<FormControl>
+					<Trigger />
+				</FormControl>
+			) : (
+				<Trigger />
+			)}
 			<SelectContent>
 				{items.map(({ label, value }) => (
 					<SelectItem value={value}>{label}</SelectItem>
