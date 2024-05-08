08fe16fabb42d53718fffd4be1d1839a4aff6521
daigoro <daigoro.abe@systemi.co.jp>
2024-05-08 10:12:09 +0900

    feat(back): ユーザ登録時に accounts テーブルも同時に更新するように変更

--- src/features/auth/routes/server/signUp/signUp.repository.ts
+++ src/features/auth/routes/server/signUp/signUp.repository.ts
@@ -1,15 +1,27 @@
-import { users } from "@/db/schema";
-import type { ISignUp } from "./signUp.service";
+import { accounts, users } from "@/db/schema";
+import type { ISignUp, Provider, User } from "./signUp.service";
 import { type DrizzleD1Database, drizzle } from "drizzle-orm/d1";
-import type { InferInsertModel } from "drizzle-orm";
-
+import { eq, desc } from "drizzle-orm";
 export default class SignUpRepository implements ISignUp {
 	private db: DrizzleD1Database;
 	constructor(connection: D1Database) {
 		this.db = drizzle(connection);
 	}
 
-	async createUser(user: InferInsertModel<typeof users>): Promise<void> {
-		await this.db.insert(users).values(user);
+	async signUp(user: User, provider: Provider): Promise<void> {
+		const lastUserId = await this.db
+			.select({ id: users.id })
+			.from(users)
+			.orderBy(desc(users.id))
+			.limit(1);
+		const targetUserId = lastUserId[0].id + 1;
+
+		await this.db.batch([
+			this.db.insert(users).values({ id: targetUserId, ...user }),
+			this.db.insert(accounts).values({
+				userId: targetUserId,
+				...provider,
+			}),
+		]);
 	}
 }
