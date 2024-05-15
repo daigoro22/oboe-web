19cf35c4645ccaae1e2ce44dd7abc19e1a56dde2
daigoro <daigoro.abe@systemi.co.jp>
2024-05-16 07:24:34 +0900

    feat(db): ankisesions, decks, cards を追加するマイグレーション

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,6 +2,7 @@ import { relations, sql } from "drizzle-orm";
 import {
   foreignKey,
   integer,
+  real,
   sqliteTable,
   text,
   unique,
@@ -77,3 +78,70 @@ export const objectives = sqliteTable("objectives", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name").notNull(),
 });
+
+export const ankiSessions = sqliteTable(
+  "AnkiSessions",
+  {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    deckId: text("deckId").notNull(),
+    userId: text("userId").notNull(),
+    startsAt: integer("startsAt", { mode: "timestamp_ms" }).notNull(),
+    endsAt: integer("endsAt", { mode: "timestamp_ms" }).notNull(),
+    deckName: text("deckName").notNull(),
+  },
+  (ankiSessions) => ({
+    deckFk: foreignKey({
+      columns: [ankiSessions.deckId],
+      foreignColumns: [decks.id],
+    }),
+    userFk: foreignKey({
+      columns: [ankiSessions.userId],
+      foreignColumns: [users.id],
+    }),
+  }),
+);
+
+export const decks = sqliteTable(
+  "Decks",
+  {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    userId: text("userId").notNull(),
+    name: text("name").notNull(),
+    description: text("description").notNull(),
+  },
+  (decks) => ({
+    userFk: foreignKey({
+      columns: [decks.userId],
+      foreignColumns: [users.id],
+    }),
+  }),
+);
+
+export const cards = sqliteTable(
+  "Cards",
+  {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    deckId: text("deckId").notNull(),
+    number: integer("number").notNull(),
+    frontContent: text("frontContent").notNull(),
+    backContent: text("backContent").notNull(),
+    stability: real("stability").notNull(),
+    difficulty: real("difficulty").notNull(),
+    due: integer("due", { mode: "timestamp_ms" }).notNull(),
+    elapsedDays: integer("elapsedDays").notNull(),
+    lastElapsedDays: integer("lastElapsedDays").notNull(),
+    scheduledDays: integer("scheduledDays").notNull(),
+    review: integer("review", { mode: "timestamp_ms" }).notNull(),
+    duration: integer("duration").notNull(),
+    lat: real("lat").notNull(),
+    lng: real("lng").notNull(),
+    pitch: real("pitch").notNull(),
+    heading: real("heading").notNull(),
+  },
+  (cards) => ({
+    deckFk: foreignKey({
+      columns: [cards.deckId],
+      foreignColumns: [decks.id],
+    }),
+  }),
+);
