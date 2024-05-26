5473555cf9a9f1b7b0545b3a51e2957a5ca6d60d
daigoro <daigoro.abe@systemi.co.jp>
2024-05-27 08:00:58 +0900

    feat(back): 暗記セッション新規作成と取得のエンドポイントを追加

--- src/features/ankiSession/routes/server/ankiSession/ankiSession.controller.ts
+++ src/features/ankiSession/routes/server/ankiSession/ankiSession.controller.ts
@@ -3,9 +3,11 @@ import AnkiSessionService, {
   type SessionAndPoint,
 } from "./ankiSession.service";
 import type { Env } from "env";
-import { Hono } from "hono";
-import { createMiddleware } from "hono/factory";
+import { type Context as C, Hono } from "hono";
+import { createFactory, createMiddleware } from "hono/factory";
 import { container } from "tsyringe";
+
+type Context = C<Env>;
 
 const ROUTE = "/api/auth/verified/ankiSession" as const;
 
@@ -18,7 +20,9 @@ export const ankiSessionContainerMiddleware = createMiddleware(
   },
 );
 
-export const ankiSession = new Hono<Env>().get(`${ROUTE}/latest`, async (c) => {
+const factory = createFactory();
+
+const latestGet = factory.createHandlers(async (c: Context) => {
   const ankiSession = container.resolve(AnkiSessionService);
   const user = c.get("userData");
   let sessionAndPoint: SessionAndPoint;
@@ -29,3 +33,31 @@ export const ankiSession = new Hono<Env>().get(`${ROUTE}/latest`, async (c) => {
   }
   return c.json(sessionAndPoint);
 });
+
+const newPost = factory.createHandlers(async (c: Context) => {
+  const ankiSession = container.resolve(AnkiSessionService);
+  const user = c.get("userData");
+  const newSession = await ankiSession.startSession(user);
+  return c.json({ sessionId: newSession });
+});
+
+const idGet = factory.createHandlers(async (c: Context) => {
+  const ankiSession = container.resolve(AnkiSessionService);
+  const user = c.get("userData");
+  const id = c.req.param("id");
+  let session: Awaited<ReturnType<typeof ankiSession.getSessionById>>;
+  try {
+    session = await ankiSession.getSessionById(user.id, id);
+    if (!session) {
+      return c.json({ error: "not found" }, 404);
+    }
+  } catch (e) {
+    return c.json({ error: "server error" }, 500);
+  }
+  return c.json({ sessionId: session.id });
+});
+
+export const ankiSession = new Hono<Env>()
+  .get(`${ROUTE}/latest`, ...latestGet)
+  .post(`${ROUTE}/new`, ...newPost)
+  .get(`${ROUTE}/:id`, ...idGet);
