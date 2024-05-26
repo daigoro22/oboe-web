853eb8a02444181c414704d7619ee3dd07dadec6
daigoro <daigoro.abe@systemi.co.jp>
2024-05-27 07:59:23 +0900

    feat(db): ankiSession.repositroy にセッション開始とセッション取得関数を追加

--- src/features/ankiSession/routes/server/ankiSession/ankiSession.repository.ts
+++ src/features/ankiSession/routes/server/ankiSession/ankiSession.repository.ts
@@ -2,6 +2,7 @@ import { ankiSessions, users } from "@/db/schema";
 import type { IAnkiSession, SessionAndPoint } from "./ankiSession.service";
 import { type DrizzleD1Database, drizzle } from "drizzle-orm/d1";
 import { desc, eq } from "drizzle-orm";
+import { nanoid } from "nanoid";
 
 export default class AnkiSessionRepository implements IAnkiSession {
   private db: DrizzleD1Database;
@@ -23,5 +24,38 @@ export default class AnkiSessionRepository implements IAnkiSession {
         .limit(1)
     )[0] ?? { point: undefined, session: undefined };
     return latestSessionAndPoint;
+  }
+
+  async createSessionAndUpdatePoint(
+    userId: number,
+    deckPublicId: string,
+    point: number,
+  ) {
+    const publicId = nanoid();
+    await this.db.batch([
+      this.db.insert(ankiSessions).values({
+        userId,
+        deckPublicId,
+        startsAt: new Date(),
+        endsAt: null,
+        isResumable: 1,
+        resumeCount: 0,
+        publicId,
+      }),
+      this.db.update(users).set({ point }).where(eq(users.id, userId)),
+    ]);
+    return publicId;
+  }
+
+  async getSessionById(publicId: string) {
+    const data = (
+      await this.db
+        .select()
+        .from(ankiSessions)
+        .where(eq(ankiSessions.publicId, publicId))
+        .limit(1)
+    )[0];
+
+    return data ?? undefined;
   }
 }
