a3c4a05eeff036dd82a9aa9f65b42a5bcb23ea72
daigoro <daigoro.abe@systemi.co.jp>
2024-06-09 18:03:55 +0900

    feat(front): トップ画面に学習継続日数コンポーネントを追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,16 @@
+import { CardLayout } from "@/components/elements/CardLayout";
+import { Grid } from "@/components/elements/Grid";
+import { AnkiSessionDrawer } from "@/features/ankiSession/components/AnkiSessionDrawer";
+import { StudyStreakCard } from "@/features/ankiSession/components/StudyStreakCard";
+
 const Index = () => {
-  return <></>;
+  return (
+    <Grid>
+      <div className="col-span-2">
+        <StudyStreakCard streak={1} />
+      </div>
+    </Grid>
+  );
 };
 
 export default Index;
