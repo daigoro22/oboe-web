654cec7740a9438648b617fa324ac87ad67a9fb3
daigoro <daigoro.abe@systemi.co.jp>
2024-06-18 05:26:28 +0900

    feat(front): 学習中断ボタンを押した際の処理を追加

--- src/features/ankiSession/components/AbortButton/AbortButton.tsx
+++ src/features/ankiSession/components/AbortButton/AbortButton.tsx
@@ -1,18 +1,61 @@
+import { Flex } from "@/components/elements/Flex";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogOverlay,
+  AlertDialogPortal,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import { Button, type ButtonProps } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { BookOpenCheck } from "lucide-react";
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
 export type AbortButtonProps = {} & ButtonProps;
 export const AbortButton = (props: AbortButtonProps) => {
+  const [abortOpen, setAbortOpen] = React.useState(false);
+  const navigate = useNavigate();
+
   return (
-    <Button
-      {...props}
-      variant="destructive"
-      className={cn("flex gap-2 px-3 py-1", props.className)}
-    >
-      <BookOpenCheck className="w-4 h-4" />
-      学習を終了する
-    </Button>
+    <>
+      <AlertDialog open={abortOpen} onOpenChange={setAbortOpen}>
+        <AlertDialogPortal>
+          <AlertDialogOverlay />
+          <AlertDialogContent
+            onEscapeKeyDown={(event) => event.preventDefault()}
+          >
+            <AlertDialogTitle>学習中断</AlertDialogTitle>
+            <AlertDialogDescription>
+              学習を中断してトップ画面に戻りますか？
+            </AlertDialogDescription>
+            <Flex
+              direction="row"
+              justifyContent="center"
+              gap="md"
+              alignItems="end"
+            >
+              <AlertDialogAction onClick={() => navigate("/")}>
+                中断
+              </AlertDialogAction>
+              <AlertDialogCancel>キャンセル</AlertDialogCancel>
+            </Flex>
+          </AlertDialogContent>
+        </AlertDialogPortal>
+      </AlertDialog>
+
+      <Button
+        {...props}
+        variant="destructive"
+        className={cn("flex gap-2 px-3 py-1", props.className)}
+        onClick={() => setAbortOpen(true)}
+      >
+        <BookOpenCheck className="w-4 h-4" />
+        学習を終了する
+      </Button>
+    </>
   );
 };
