6f4a919e9c063d35d539bc48f39f8d122037c8c0
daigoro <daigoro.abe@systemi.co.jp>
2024-06-18 21:59:47 +0900

    feat(front): ストリートビューのコンポーネントを lazy loading するようにした

--- src/features/ankiSession/components/StreetView/StreetView.tsx
+++ src/features/ankiSession/components/StreetView/StreetView.tsx
@@ -1,64 +1,18 @@
 import * as React from "react";
-import {
-  APIProvider,
-  Map as GoogleMap,
-  useMap,
-} from "@vis.gl/react-google-maps";
-import {
-  isErrorResponse,
-  resumeSessionAtom,
-} from "@/features/ankiSession/atoms/ankiSessionAtom";
-import { useAtomValue } from "jotai";
-import { targetCardNumAtom } from "@/features/ankiSession/atoms/ankiSessionAtom";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useAtomValue } from "jotai";
+import { resumeSessionAtom } from "@/features/ankiSession/atoms/ankiSessionAtom";
 
-export type StreetViewProps = {
-  position: { lat: number; lng: number };
-  pov: { heading: number; pitch: number };
-};
-
-const View = ({ position, pov }: StreetViewProps) => {
-  const map = useMap("main");
-  const panorama = map?.getStreetView();
-  panorama?.setPosition(position);
-  panorama?.setPov(pov);
-  panorama?.setVisible(true);
-
-  return (
-    <GoogleMap
-      id="main"
-      defaultZoom={3}
-      defaultCenter={{ lat: 0, lng: 0 }}
-      gestureHandling="none"
-      disableDefaultUI={true}
-    />
-  );
-};
+const Core = React.lazy(
+  () => import("@/features/ankiSession/components/StreetView/Core"),
+);
 
 export const StreetView = () => {
-  const apiKey = React.useMemo(
-    () => import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-    [],
-  );
-  const { data: d, isSuccess } = useAtomValue(resumeSessionAtom);
-  const data = isErrorResponse(d) ? undefined : d;
-  const targetCardNum = useAtomValue(targetCardNumAtom);
-
-  const position = {
-    lat: data?.cards[targetCardNum]?.lat ?? 0,
-    lng: data?.cards[targetCardNum]?.lng ?? 0,
-  };
-
-  const pov = {
-    heading: data?.cards[targetCardNum]?.heading ?? 0,
-    pitch: data?.cards[targetCardNum]?.pitch ?? 0,
-  };
+  const { isSuccess } = useAtomValue(resumeSessionAtom);
 
   return !isSuccess ? (
     <Skeleton className="w-full h-full" /> //TODO: Suspense 使いたいな
   ) : (
-    <APIProvider apiKey={apiKey}>
-      <View position={position} pov={pov} />
-    </APIProvider>
+    <Core />
   );
 };
