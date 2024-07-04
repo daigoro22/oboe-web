04e61f3ee295041f5dcb3fb4e803ca871470f5e0
daigoro <daigoro.abe@systemi.co.jp>
2024-07-04 09:47:42 +0900

    feat(back): アイテム購入のエンドポイントを追加

--- src/features/purchase/routes/server/purchase/purchase.repository.ts
+++ src/features/purchase/routes/server/purchase/purchase.repository.ts
@@ -14,4 +14,21 @@ export default class PurchaseRepository implements IPurchase {
     });
     return prices.data;
   }
+
+  async purchase(
+    priceId: string,
+    quantity: number,
+  ): Promise<Stripe.Checkout.Session> {
+    const origin = import.meta.env.VITE_ORIGIN;
+    const session = await this.stripe.checkout.sessions.create({
+      mode: "payment",
+      success_url: `${origin}/success`,
+      cancel_url: `${origin}/cancel`,
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      line_items: [{ price: priceId, quantity }],
+    });
+
+    return session;
+  }
 }
--- src/features/purchase/routes/server/purchase/purchase.service.ts
+++ src/features/purchase/routes/server/purchase/purchase.service.ts
@@ -3,14 +3,29 @@ import { inject, injectable } from "tsyringe";
 
 export interface IPurchase {
   getAllProductsAndPrices(): Promise<Stripe.Price[]>;
+  purchase(priceId: string, quantity: number): Promise<Stripe.Checkout.Session>;
 }
 
 @injectable()
 export default class PurchaseService {
-  constructor(@inject("IPurchase") private purchase: IPurchase) {}
+  constructor(@inject("IPurchase") private _purchase: IPurchase) {}
 
   async getAllProductsAndPrices() {
-    const products = await this.purchase.getAllProductsAndPrices();
+    const products = await this._purchase.getAllProductsAndPrices();
     return products;
+  }
+
+  async purchase(priceId: string, quantity: string | undefined) {
+    if (!(quantity && !Number.isNaN(Number(quantity)))) {
+      throw new InvalidQuantityError();
+    }
+    const session = await this._purchase.purchase(priceId, Number(quantity));
+    return session;
+  }
+}
+
+export class InvalidQuantityError extends Error {
+  constructor() {
+    super("Invalid quantity");
   }
 }
