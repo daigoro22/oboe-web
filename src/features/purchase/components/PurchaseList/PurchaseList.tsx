1a1dd9f2456883435189cab5b46fd45e08c1fc83
daigoro <daigoro.abe@systemi.co.jp>
2024-07-20 09:31:41 +0900

    feat(front): purchase/ で全てのアイテムのリスト表示を行った

--- src/features/purchase/components/PurchaseList/PurchaseList.tsx
+++ src/features/purchase/components/PurchaseList/PurchaseList.tsx
@@ -1,8 +1,11 @@
 import { List } from "@/components/elements/List";
+import { Button } from "@/components/ui/button";
 import {
   type AllProductsAndPricesResponse,
   getAllProductsAndPrices,
 } from "@/features/purchase/api/purchase";
+import { Coins, JapaneseYen } from "lucide-react";
+import type { ComponentProps } from "react";
 import { useLoaderData } from "react-router-dom";
 
 // biome-ignore lint/complexity/noBannedTypes: <explanation></explanation>
@@ -11,28 +14,40 @@ export const purchaseLoader = async () => await getAllProductsAndPrices();
 
 export const PurchaseList = (props: PurchaseListProps) => {
   const allProductsAndPrices = useLoaderData() as AllProductsAndPricesResponse; //FIXME: as を使わず型付け
-  console.log(allProductsAndPrices);
 
-  return (
-    <List
-      rows={[
-        {
-          key: "row1",
-          cells: [
-            { key: "cell1", content: "Cell 1" },
-            { key: "cell2", content: "Cell 2" },
-            { key: "cell3", content: "Cell 3" },
-          ],
-        },
-        {
-          key: "row2",
-          cells: [
-            { key: "cell3", content: "Cell 3" },
-            { key: "cell4", content: "Cell 4" },
-            { key: "cell5", content: "Cell 5" },
-          ],
-        },
-      ]}
-    />
+  const rows = allProductsAndPrices.reduce(
+    (
+      acc: ComponentProps<typeof List>["rows"],
+      { product, id, unit_amount },
+    ) => {
+      if (typeof product !== "string" && (product.deleted || !product.name)) {
+        return acc;
+      }
+      acc.push({
+        key: id,
+        cells: [
+          { key: "icon", content: <Coins /> },
+          {
+            key: "point",
+            content: typeof product === "string" ? product : product.name ?? "",
+          },
+          {
+            key: "button",
+            content: (
+              <Button>
+                <>
+                  <JapaneseYen />
+                  {unit_amount}
+                </>
+              </Button>
+            ),
+          },
+        ],
+      });
+      return acc;
+    },
+    [],
   );
+
+  return <List rows={rows} />;
 };
